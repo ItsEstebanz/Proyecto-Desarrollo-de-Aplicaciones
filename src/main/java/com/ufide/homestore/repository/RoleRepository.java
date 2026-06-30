bc429a48245ca60b5d178b0d5562a4ffcd839757
Esteban Solis <232974029+esteban-solis@users.noreply.github.com>
2026-06-30 03:55:37 -0600

    feat(repository): add JPA repository interfaces

---
+++ src/main/java/com/ufide/homestore/repository/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.ufide.homestore.repository;
+
+import com.ufide.homestore.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {}
---
+++ src/main/java/com/ufide/homestore/repository/SupplierRepository.java
@@ -0,0 +1,6 @@
+package com.ufide.homestore.repository;
+
+import com.ufide.homestore.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {}
